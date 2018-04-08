5d999264304742ae87ef5d026b2b38b2b186889c
crlang <crlang@163.com>
2018-04-08 14:58:26 +0800

    change&fix catelog style

--- pages/catalog/catalog.js
+++ pages/catalog/catalog.js
@@ -9,13 +9,13 @@ Page({
     categories: [],
     childCategories: [],
     curId: 0,
-    srollHeight:300//需要动态获取屏幕高度，给scorllview高度赋值，随便用一个值初始化数据
+    srollHeight: 300 //需要动态获取屏幕高度，给scorllview高度赋值，随便用一个值初始化数据
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     wx.setNavigationBarTitle({
       title: util.pageTitle.catalog
     });
@@ -38,6 +38,8 @@ Page({
       page: 1,
       per_page: 999
     }).then(res => {
+      console.log(res.categories);
+
       this.setData({
         categories: res.categories,
         childCategories: res.categories[0].categories,
@@ -47,71 +49,73 @@ Page({
     });
   },
 
+  //事件处理函数
+  switchRightTab(e) {
+    // 获取item项的id，和数组的下标值
+    let id = e.target.dataset.id,
+      index = parseInt(e.target.dataset.index);
+    console.log(index,id);
+
+    // 把点击到的某一项，设为当前index
+    this.setData({
+      curId: id,
+      childCategories: this.data.categories[index].categories
+    });
+  },
+
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-    var that = this;
-     wx.getSystemInfo({
-      success: function (res) {
-        var height = res.windowHeight - 51;   //footerpannelheight为底部组件的高度
-        that.setData({
+  onShow: function() {
+    var self = this;
+    wx.getSystemInfo({
+      success: function(res) {
+        var height = res.windowHeight - 50; //footerpannelheight为底部组件的高度
+        self.setData({
           srollHeight: height
         });
       }
-     });
+    });
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   },
-
-  //事件处理函数  
-  switchRightTab: function (e) {
-  // 获取item项的id，和数组的下标值  
-  let id = e.target.dataset.id,
-  index = parseInt(e.target.dataset.index);
-  // 把点击到的某一项，设为当前index  
-  this.setData({
-      curId: id,
-      childCategories: this.data.categories[index].categories
-    });
-  }
 });
